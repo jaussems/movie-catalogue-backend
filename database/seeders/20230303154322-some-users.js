26a87cc0e118261a6cb2b944d63ae0ce93859213
jaussems <jannesaussems@gmail.com>
2023-03-03 16:56:00 +0100

    seeding , migrating testing sequelize again

---
+++ database/seeders/20230303154322-some-users.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          name: "Leo Messi",
+          email: "leo@messi.com",
+          phone: 1234567,
+          password: "test",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Dan Abramov",
+          email: "dan@redux.com",
+          phone: 1234567,
+          password: "test",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("users", null, {});
+  }
+};
